987f9b6a6d4bbc466ea9c9d79c8d4933a16ebe58
RedContritio <RedContritio@qq.com>
2023-05-07 10:07:28 +0000

    support add character to userboard

--- gicg_sim/userboard.py
+++ gicg_sim/userboard.py
@@ -1,6 +1,12 @@
+from .utils.card import Card
+
+
 class UserBoard:
     def __init__(self) -> None:
         self.charaters = []
         self.hand = []
         self.dies = {}
         self.effects = []
+
+    def add_character(self, name: str):
+        self.charaters.append(Card.create_character(name))
---
+++ tests/test_userboard.py
@@ -0,0 +1,15 @@
+import unittest
+
+from gicg_sim.userboard import UserBoard
+
+
+class TestUserBoard(unittest.TestCase):
+    def test_add_character(self):
+        userboard = UserBoard()
+        userboard.add_character("Diluc")
+        self.assertEqual(len(userboard.charaters), 1)
+        self.assertEqual(userboard.charaters[0]["name"], "Diluc")
+
+
+if __name__ == "__main__":
+    unittest.main()
