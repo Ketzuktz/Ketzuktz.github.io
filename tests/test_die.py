80494046cf12973b420c30356ad6931fe0797f93
RedContritio <RedContritio@qq.com>
2023-05-10 09:26:12 +0000

    update test_die and pre-commit

--- tests/test_die.py
+++ tests/test_die.py
@@ -1,25 +1,108 @@
 import random
 import unittest
 
-from gicg_sim.die import create_die_state, element_names
+from gicg_sim.die import DieState, element_names
+
+
+def create_random_die(low: int = 0, high: int = 10):
+    kwargs = dict((en, random.randint(low, high)) for en in element_names)
+    return DieState(**kwargs)
 
 
 class TestDieState(unittest.TestCase):
-    def test_create_die_state(self):
-        for _ in range(10):
-            die_count = dict([(en, 0) for en in element_names])
+    def setUp(self):
+        self.die_state = DieState()
 
-            die_total = random.randint(8, 20)
-            for i in range(die_total):
-                en = random.choice(element_names)
-                die_count[en] = die_count[en] + 1
+    def test_omni(self):
+        self.die_state.Omni = 5
+        self.assertEqual(self.die_state.Omni, 5)
 
-            kwargs = dict([(k.lower(), v) for k, v in die_count.items()])
-            die_state = create_die_state(**kwargs)
+    def test_pyro(self):
+        self.die_state.Pyro = 3
+        self.assertEqual(self.die_state.Pyro, 3)
 
-            self.assertEqual(sum(die_state.die_count), die_total)
-            for en in element_names:
-                self.assertEqual(getattr(die_state, en), die_count[en])
+    def test_hydro(self):
+        self.die_state.Hydro = 2
+        self.assertEqual(self.die_state.Hydro, 2)
+
+    def test_anemo(self):
+        self.die_state.Anemo = 4
+        self.assertEqual(self.die_state.Anemo, 4)
+
+    def test_electro(self):
+        self.die_state.Electro = 1
+        self.assertEqual(self.die_state.Electro, 1)
+
+    def test_dendro(self):
+        self.die_state.Dendro = 0
+        self.assertEqual(self.die_state.Dendro, 0)
+
+    def test_cryo(self):
+        self.die_state.Cryo = 2
+        self.assertEqual(self.die_state.Cryo, 2)
+
+    def test_geo(self):
+        self.die_state.Geo = 6
+        self.assertEqual(self.die_state.Geo, 6)
+
+    def test_addition(self):
+        die_state1 = DieState(Omni=2, Pyro=3, Hydro=1)
+        die_state2 = DieState(Omni=1, Pyro=2, Hydro=4)
+        result = die_state1 + die_state2
+        self.assertEqual(result.Omni, 3)
+        self.assertEqual(result.Pyro, 5)
+        self.assertEqual(result.Hydro, 5)
+
+    def test_subtraction_1(self):
+        die_state1 = DieState(Omni=5, Pyro=3, Hydro=2)
+        die_state2 = DieState(Omni=2, Pyro=1, Hydro=1)
+        result = die_state1 - die_state2
+        self.assertEqual(result.Omni, 3)
+        self.assertEqual(result.Pyro, 2)
+        self.assertEqual(result.Hydro, 1)
+
+    def test_subtraction_2(self):
+        die_state1 = DieState(Omni=5, Pyro=3, Hydro=2)
+        die_state2 = DieState(Omni=2, Pyro=1, Hydro=1)
+        result = die_state1 - die_state2
+        self.assertEqual(result.Omni, 3)
+        self.assertEqual(result.Pyro, 2)
+        self.assertEqual(result.Hydro, 1)
+
+    def test_subtraction_exception(self):
+        DieState(Omni=2, Pyro=3, Hydro=1)
+        DieState(Omni=4, Pyro=2, Hydro=3)
+        with self.assertRaises(AssertionError):
+            pass
+
+    def test_comparison(self):
+        die_state1 = create_random_die()
+
+        for en in element_names:
+            die_state2 = die_state1.copy()
+            value = getattr(die_state2, en)
+            setattr(die_state2, en, value + 1)
+
+            self.assertFalse(die_state1 > die_state2)
+            self.assertFalse(die_state1 < die_state2)
+            self.assertLessEqual(die_state1, die_state2)
+
+        die_state2 = die_state1.copy()
+        for i in range(len(die_state2.die_count)):
+            die_state2.die_count[i] = die_state2.die_count[i] + 1
+
+        for en in element_names:
+            die_state3 = die_state2.copy()
+            value = getattr(die_state3, en)
+            setattr(die_state3, en, value + 1)
+
+            self.assertFalse(die_state1 > die_state2)
+            self.assertLess(die_state1, die_state2)
+            self.assertLessEqual(die_state1, die_state2)
+
+    def test_invalid_die_count(self):
+        with self.assertRaises(AssertionError):
+            self.die_state.Omni = -1
 
 
 if __name__ == "__main__":
