b3e5c55c13efd9686d3060fe9f04c2ac2bb777e1
RedContritio <RedContritio@qq.com>
2023-06-08 09:22:11 +0000

    add package-data config

--- gicg_sim/__init__.py
+++ gicg_sim/__init__.py
@@ -1,19 +1,14 @@
-import pkgutil
 import sys
-from importlib.abc import Loader
-from typing import Optional
 
 __version__ = "0.0.1"
 
 if sys.version_info < (3, 7):
     raise RuntimeError("gicg_sim requires Python 3.7 or later")
 
-__all__ = []
+from . import basic, card, utils
 
-for loader, module_name, is_pkg in pkgutil.walk_packages(__path__):
-    __all__.append(module_name)
-    __loader__: Optional[Loader] = \
-        loader.find_module(module_name)  # type: ignore [call-arg]
-    if __loader__ is not None:
-        _module = __loader__.load_module(module_name)
-        globals()[module_name] = _module
+__all__ = [
+    'card',
+    'basic',
+    'utils',
+]
