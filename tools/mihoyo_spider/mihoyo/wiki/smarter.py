c0813753b992894a0d374af22c663cc754c4ff0a
RedContritio <RedContritio@qq.com>
2023-10-16 13:20:23 +0800

    stash test extract skill data

--- tools/mihoyo_spider/mihoyo/wiki/smarter.py
+++ tools/mihoyo_spider/mihoyo/wiki/smarter.py
@@ -1,5 +1,6 @@
 from enum import Enum
 
+from lxml import etree
 from mihoyo.wiki.card import CardInfo
 
 from gicg_sim.model.prototype.character import CharacterPrototype as CP
@@ -9,15 +10,17 @@ from gicg_sim.model.prototype.skill import SkillPrototype as SP
 class DictContext:
     def __init__(self) -> None:
         self._dictionary = {}
+        self._raw_data = {}
         pass
 
-    def update(self, key: str, value: str):
+    def update(self, key: str, value: str, raw_data: str = ""):
         if key in self._dictionary:
             if self._dictionary[key] != value:
                 raise RuntimeError(
                     f"Conflict: {key} = {self._dictionary[key]} and {value}"
                 )
         self._dictionary[key] = value
+        self._raw_data[key] = raw_data
 
     @property
     def data(self) -> dict:
@@ -30,6 +33,34 @@ class CardInfoType(Enum):
     monster = "魔物牌"
 
 
+def _extract_skill_comment(skill_desc: str, context: DictContext) -> str:
+    root = etree.fromstring(skill_desc)
+
+    # 获取包含技能描述的整个字符串，但排除 "[详情]" 部分
+    skill_description = "".join(
+        root.xpath('//*[not(self::span[@data-type="详情"])]/text()')
+    ).strip()
+
+    # 查找所有包含 "strong" 的文本
+    strong_elements = root.xpath(".//strong//text()")
+    for element in strong_elements:
+        # 获取加粗文本
+        bold_text = element.strip()
+        if bold_text:
+            # 检查是否有相邻的 span 元素包含正确的 data-type 和 data-name 属性
+            next_element = element.getparent().getparent().getnext()
+            if (
+                next_element is not None
+                and next_element.tag == "span"
+                and next_element.get("data-type") == "详情"
+            ):
+                # 获取详情文本
+                detail_text = next_element.get("data-name")
+                context.update(bold_text, detail_text, skill_desc)
+
+    return skill_description
+
+
 def _extract_character_data(card: CardInfo, context: DictContext) -> CP:
     skills: list[SP] = [
         {
@@ -37,17 +68,20 @@ def _extract_character_data(card: CardInfo, context: DictContext) -> CP:
             "type": s["skill_type"],
             "cost": s["costs"],
             "effect": s["desc"],
-            "comment": s["desc"],
+            "comment": _extract_skill_comment(s["desc"], context),
         }
         for s in card["skills"]
     ]
+
+    assert len(card["base_info"]["element"]) == 1
+    assert len(card["base_info"]["weapon"]) == 1
     output: CP = {
         "name": card["name"],
         "rariry": -1,
-        "element": card["base_info"]["element"],
-        "weapon": card["base_info"]["weapon"],
+        "element": card["base_info"]["element"][0],
+        "weapon": card["base_info"]["weapon"][0],
         "max_HP": card["base_info"]["life"],
-        "max_energy": card["base_info"]["energy_count"],
+        "max_energy": card["base_info"]["energy"],
         "faction": card["base_info"]["faction"],
         "skills": skills,
     }
---
+++ tools/mihoyo_spider/test.py
@@ -0,0 +1,46 @@
+from lxml import etree
+
+html_string = '<p style="white-space: pre-wrap;">造成2点\
+<strong><u>物理伤害</u></strong>\
+<span data-type="详情" data-name="&lt;p style=&#34;\
+white-space: pre-wrap;&#34;&gt;\
+&lt;strong&gt;物理伤害&lt;/strong&gt;\
+&lt;/p&gt;&lt;p style=&#34;white-space: pre-wrap;&#34;&gt;\
+物理伤害不会附着元素，也不会发生元素反应。\
+&lt;/p&gt;" class="wiki-note-text">\
+<sup class="wiki-sup">[详情]</sup></span>。</p>'
+
+# 创建lxml Element对象
+root = etree.fromstring(html_string)
+
+# 获取包含技能描述的整个字符串，但排除 "[详情]" 部分
+skill_description = "".join(
+    root.xpath('//*[not(self::span[@data-type="详情"])]/text()')
+).strip()
+print("技能描述:", skill_description)
+
+# 创建词典来存储加粗文本和详情
+bold_text_dict = {}
+
+# 查找所有包含 "strong" 的文本
+strong_elements = root.xpath(".//strong//text()")
+for element in strong_elements:
+    # 获取加粗文本
+    bold_text = element.strip()
+    if bold_text:
+        # 检查是否有相邻的 span 元素包含正确的 data-type 和 data-name 属性
+        next_element = element.getparent().getparent().getnext()
+        if (
+            next_element is not None
+            and next_element.tag == "span"
+            and next_element.get("data-type") == "详情"
+        ):
+            # 获取详情文本
+            detail_text = next_element.get("data-name")
+            bold_text_dict[bold_text] = detail_text
+
+# 打印技能描述和词典
+print("技能描述:", skill_description)
+print("加粗文本和详情:")
+for bold_text, detail_text in bold_text_dict.items():
+    print(f"{bold_text}: {detail_text}")
